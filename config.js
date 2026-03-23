3a925fa62551f6f60ff362b2064b20a118265233
NgEngKi-edu <jigo116@go.edu.tw>
2026-03-23 22:45:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.HW_UPLOAD_CONFIG = {
   // 請貼上你部署好的 GAS Web App /exec 網址
-  GAS_WEB_APP_URL: 'PASTE_YOUR_GAS_WEB_APP_EXEC_URL_HERE'
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxvDpfNJxmmfc0JynOmOGx-DdzzToLokL4wl0ac_ZLoyOAQQoW0kCI3eWZqcWGfimA/exec'
 };
