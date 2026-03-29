c493bc82fc30cd8631fbc6bc4ed6c20bef995dee
NgEngKi-edu <jigo116@go.edu.tw>
2026-03-29 23:28:47 +0800

    Update config.js
    班級座號格式

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.HW_UPLOAD_CONFIG = {
   // 請貼上你部署好的 GAS Web App /exec 網址
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxth_S2C9AfjISDPJYEWp7dYjFkvBCeykgUm61Np6blH3HvlXwuo8Th4xO88pEUBYo/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbynDxuhiltxdDGEZeMMoyLBaWk4KcBEzW9gpeIO2wNyQBlfqg8PhdpN55NtDaJV3_c/exec',
 
   // 預設整體字體倍率：1 = 100%，1.1 = 110%，0.95 = 95%
   DEFAULT_FONT_SCALE: 0.8
