7aaef1002aaab21ee3f5ff4fd9bca135cb5a964c
NgEngKi-edu <jigo116@go.edu.tw>
2026-04-06 22:51:29 +0800

    Update config.js
    V04062250

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.HW_UPLOAD_CONFIG = {
   // 請貼上你部署好的 GAS Web App /exec 網址
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwCpLqaVYynqrqyZjq88pkMUJZJLHMb8qWDhoaVI_X7CAu4NgjpjVw-ChPSzy9bo5M/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycby8pPf47ca5S-auNJs2MJIOOcuwJ8Gc3V7dkhmSpWik9oo4S1unQF4KvvkM0NxLm-w/exec',
 
   // 預設整體字體倍率：1 = 100%，1.1 = 110%，0.95 = 95%
   DEFAULT_FONT_SCALE: 0.8
